2ac0dc55b2e3b39f39f82733b94b91377e8d6fd3
unknown <iyandataofeek11@gmail.com>
2022-09-06 09:28:19 +0100

    done

---
+++ src/components/AllinOne.jsx
@@ -0,0 +1,98 @@
+import { CheckIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+const AllinOne = () => {
+  return (
+    <div name="platform" className='w-full my-32'>
+            <div className='max-w-[1240px] mx-auto px-2'>
+                <h2 className='text-5xl font-bold text-center'>All-In One Platform</h2>
+                <p className='text-2xl py-8 text-gray-500 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque vero sint recusandae? Nesciunt suscipit dolorem necessitatibus quidem? Ex nobis unde, natus sunt omnis eos, quae a cupiditate optio ea rem?</p>
+            
+            <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+                <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600'/>
+                        </div>                    
+                        <div className=''>
+                            <h3 className='font-bold text-lg'>Notification</h3>
+                            <p className='text-lg pt-2 pb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod ducimus maxime voluptas quam quia delectus aliquam, quisquam ratione enim saepe recusandae praesentium nesciunt provident dolor? 
+                            Voluptas quidem maxime vel nam.</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AllinOne
